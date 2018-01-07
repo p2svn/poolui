cb7ef106dbc9363463c1c50c53af5933ca9c30e1
p2svn <33637233+p2svn@users.noreply.github.com>
2018-01-08 04:08:32 +0700

    Update globals.js

--- app/globals.js
+++ app/globals.js
@@ -4,8 +4,8 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "XMRPool.net",
-		api_url : 'https://api.xmrpool.net',
+		pool_name: "p2s.com.vn",
+		api_url : 'http://p2s.com.vn/api',
 		api_refresh_interval: 5000,
 		app_update_interval: 5*60000
 	};
